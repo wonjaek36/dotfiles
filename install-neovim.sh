3c329adff7eb04addcda5e49c7b3044f279b1e4d
Wonjae Kim <wonjaek36@gmail.com>
2022-04-07 09:53:00 +0900

    Add install-neovim.sh

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,63 +1,18 @@
-if ( command neovim --version 1> /dev/null 2> /dev/null ); then
-    echo "Neovim already installed"
-    exit 1
+if ! [ -z INSTALL_PATH ]; then
+	INSTALL_PATH="$HOME/.local"
 fi
 
-DEPENDENCIES_NEEDED=false
 
-if ! ( command make --version 1> /dev/null 2>&1 ); then
-    echo "Need to install make"
-    DEPENDENCIES_NEEDED=true
-fi
-if ! ( command cmake --version > /dev/null 2>&1 ); then
-    echo "Need to install cmake"
-    DEPENDENCIES_NEEDED=true
-fi
-if ! ( command libtool --version > /dev/null 2>&1 ); then
-    echo "Need to install libtool"
-    DEPENDENCIES_NEEDED=true
-fi
-if ! ( command git --version > /dev/null 2>&1 ); then
-    echo "Need to install git"
-    DEPENDENCIES_NEEDED=true
-fi 
+get_latest_from_github() {
+	# $1: user/repo
+    # name_tag_suffix
 
-if [ "$DEPENDENCIES_NEEDED" = true ]; then
-    
-    SUDO_PREVILEGES=$(sudo -nv 2>&1)
-    if [ $? -eq 0 ]; then
-        echo "Have sudo previlege"
-    elif echo $SUDO_PREVILEGES | grep -q '^sudo:'; then
-        echo "Have sudo, but password needed"
-        sudo -v 
-    else
-        exit 2
-    fi
-    
-    if ( command apt --version 1> /dev/null 2>&1 ); then
-        sudo apt install libtool automake cmake pkg-config gettext -y
-    elif ( command yum --version 1> /dev/null 2>&1 ); then
-        sudo yum install libtool automake cmake pkg-config gettext -y
-    else
-        echo "Unsupported Operating System"
-    fi
-fi
-if ! ( command git --version > /dev/null ); then
-	echo "Please install git first"
-	exit 3
-fi
+	curl https://api.github.com/repos/$1/releases/latest |
+		grep browser_download_url |
+		grep -Po 'https://.*?'$2 | 
+		xargs curl -L
+}
 
-if [ ! -z ${INSTALL_PATH} ]; then
-    echo "INSTALL PATH: $INSTALL_PATH"
-else
-    echo "No set INSTALL PATH"
-    exit 4
-fi
 
-# Newest version at 22/04/06 
-mkdir -p $INSTALL_PATH
-git clone -b v0.6.1 https://github.com/neovim/neovim.git $INSTALL_PATH/neovim
-cd $INSTALL_PATH/neovim
-make distclean && LDFLAGS=-static
-make CMAKE_INSTALL_PREFIX=$INSTALL_PATH
-make install
+get_latest_from_github 'neovim/neovim' 'nvim-linux64.tar.gz' 2>/dev/null | tar xz -C "$INSTALL_PATH" --strip 1
+
