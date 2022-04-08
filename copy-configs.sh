fd833e9543eb36e271a9bc3197bc3c17958c26f0
Wonjae Kim <wonjaek36@gmail.com>
2022-04-08 13:59:56 +0900

    Ignore errors

--- copy-configs.sh
+++ copy-configs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp .vimrc $HOME/.vimrc
-cp .zshrc $HOME/.zshrc
-cp .p10k.zsh $HOME/.p10k.zsh
+cp -f .vimrc $HOME/.vimrc 2>/dev/null
+cp -f .zshrc $HOME/.zshrc 2>/dev/null
+cp -f .p10k.zsh $HOME/.p10k.zsh 2>/dev/null
 
-mkdir -p $HOME/.config && cp starship.toml $HOME/.config/starship.toml
-cp .dircolors $HOME/.dircolors
+mkdir -p $HOME/.config && cp -f starship.toml $HOME/.config/starship.toml 2>/dev/null
+cp -f .dircolors $HOME/.dircolors 2>/dev/null
 
 # Nvim configuration
-mkdir -p $HOME/.config/nvim && cp init.vim $HOME/.config/nvim/init.vim
+mkdir -p $HOME/.config/nvim && cp -f init.vim $HOME/.config/nvim/init.vim 2>/dev/null
