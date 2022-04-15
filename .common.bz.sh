82fab9f0aa6822561b0baed0c784c57dfe0e2471
Wonjae Kim <wonjaek36@gmail.com>
2022-04-15 15:44:58 +0900

    PYENV common library for bash & zsh

--- .common.bz.sh
+++ .common.bz.sh
@@ -1,4 +1,12 @@
 
-if [ -f '$HOME/.common.sh' ]; then
+if [ -e "$HOME/.common.sh" ]; then
     source $HOME/.common.sh
 fi
+
+### PYENV ###
+export PYENV_HOME="$HOME/.pyenv"
+export PATH="$PYENV_HOME/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+### END OF PYENV ###
