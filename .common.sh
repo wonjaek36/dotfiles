1840acf804d2fad26a0bbe243d9a2d0e3d5547bb
Wonjae Kim <wonjaek36@gmail.com>
2022-04-22 12:53:09 +0900

    Add git diff | bat alias

--- .common.sh
+++ .common.sh
@@ -2,6 +2,7 @@
 alias gs='git status'
 alias gc='git commit -v'
 alias gd='git diff'
+alias bgd='git diff $1 | bat'
 alias ga='git add'
 alias gpo='git push origin'
 alias gpom='git push origin master'
