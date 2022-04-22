5d800e871553e8600b113a5e54319ad23d22af9a
Wonjae Kim <wonjaek36@gmail.com>
2022-04-22 16:23:59 +0900

    Destination path fix

--- install-utils.sh
+++ install-utils.sh
@@ -224,7 +224,7 @@ echo "### End ripgrep ###"
 
 echo '###### Install fd #####'
 get_latest_from_github sharkdp/fd x86_64-unknown-linux-musl.tar.gz 2> /dev/null | tar xz -C "$HOME/.local/bin" --strip 1
-mv $HOME/.local/bin/autocomplete/_fd $zsh_completion_path/_fd
-mv $HOME/.local/bin/autocomplete/fd.bash $bash_completion_path/fd
-mv $HOME/.local/bin/fd.1 $man_path/fd.1
+mv $HOME/.local/bin/autocomplete/_fd $ZSH_COMPLETION_PATH/_fd
+mv $HOME/.local/bin/autocomplete/fd.bash $BASH_COMPLETION_PATH/fd
+mv $HOME/.local/bin/fd.1 $MAN_PATH/fd.1
 
