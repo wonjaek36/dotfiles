af0a59afe492c7a7abd835e1dc8da15e80afdbc7
Wonjae Kim <wonjaek36@gmail.com>
2022-04-22 10:18:23 +0900

    Fix shell bug

--- .common.bz.sh
+++ .common.bz.sh
@@ -5,18 +5,18 @@ fi
 
 export ZSH_COMPLETION_PATH=$HOME/.local/share/zsh/vendor-completions
 export BASH_COMPLETION_PATH=$HOME/.local/share/bash/vendor-completions
-if [ -d $ZSH_COMPLETION_PATH ]; then
+if [ ! -d $ZSH_COMPLETION_PATH ]; then
     mkdir -p $ZSH_COMPLETION_PATH
 fi
-if [ -d $BASH_COMPLETION_PATH ]; then
+if [ ! -d $BASH_COMPLETION_PATH ]; then
     mkdir -p $BASH_COMPLETION_PATH
 fi
 
 if [[ ! $fpath =~ $ZSH_COMPLETION_PATH ]]; then
     fpath=($ZSH_COMPLETION_PATH $fpath)
 fi
-if [[ ! $fpath =~ $BASH_COMPLETION_PATH ]]; then
-    fpath=($BASH_COMPLETION_PATH $fpath)
+if [[ ! $FPATH =~ $BASH_COMPLETION_PATH ]]; then
+    FPATH="$BASH_COMPLETION_PATH:$FPATH"
 fi
 
 ### PYENV ###
