1303130ba064e3ffa29add210b937b17e810dfd4
Wonjae Kim <wonjaek36@gmail.com>
2022-04-22 09:17:57 +0900

    Use shared completions function directory

--- .common.bz.sh
+++ .common.bz.sh
@@ -3,6 +3,18 @@ if [ -e "$HOME/.common.sh" ]; then
     source $HOME/.common.sh
 fi
 
+ZSH_COMPLETION_PATH=$HOME/.local/share/zsh/vendor-completions
+BASH_COMPLETION_PATH=$HOME/.local/share/bash/vendor-completions
+mkdir -p $ZSH_COMPLETION_PATH
+mkdir -p $BASH_COMPLETION_PATH
+
+if [[ ! $fpath =~ $ZSH_COMPLETION_PATH ]]; then
+    fpath=($ZSH_COMPLETION_PATH $fpath)
+fi
+if [[ ! $fpath =~ $BASH_COMPLETION_PATH ]]; then
+    fpath=($BASH_COMPLETION_PATH $fpath)
+fi
+
 ### PYENV ###
 export PYENV_HOME="$HOME/.pyenv"
 export PATH="$PYENV_HOME/bin:$PATH"
@@ -10,3 +22,5 @@ eval "$(pyenv init -)"
 eval "$(pyenv init --path)"
 eval "$(pyenv virtualenv-init -)"
 ### END OF PYENV ###
+
+
